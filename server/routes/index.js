1d287d61d756fbcd38ed5b173e46a387b4015f72
reshadbinharun <md.bin_harun@tufts.edu>
2019-04-06 12:45:05 -0500

    OFI setup database with mentors table

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Creates a pool of connections so that DB connections do not have to be created 
+for every new DB call
+*/
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+}).get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM mentors');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 module.exports = router;
